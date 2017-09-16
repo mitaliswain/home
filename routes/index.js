2af565b765e8cccb4b2259daf4abc62a2cef369a
Mitali Swain <mitali.swain@gmail.com>
2017-09-15 18:35:12 -0700

    added prasad

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jagannath Lenka' });
+  res.render('index', { title: 'Jagannath Prasad nLenka' });
 
 });
 
